a5a1194488dea6194086dd75d95c2d580854e1f1
alitalu7 <55026802+alitalu7@users.noreply.github.com>
2022-06-24 10:50:40 +0300

    Add project files.

---
+++ CSharp9/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Kaç adet sayı gireceksiniz?");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] sayilar = new int[a];
+int toplam = 0;
+
+for (int i = 0; i < a; i++)
+{
+    Console.WriteLine(a + " adet sayıdan " + (i+1) + ". sayıyı giriniz..." );
+    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+}
+foreach (int say in sayilar)
+{
+    toplam = toplam + say;
+}
+
+Console.WriteLine("Girilen sayıların toplamı = " + toplam);
